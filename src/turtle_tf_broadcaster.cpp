81f9d906e8fa17adccfa2a3627f319c08a41263e
YujinHeo <blike4@kaist.ac.kr>
2019-11-19 09:31:44 +0900

    new

---
+++ src/turtle_tf_broadcaster.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <turtlesim/Pose.h>
+
+std::string turtle_name;
+
+void poseCallback(const turtlesim::PoseConstPtr& msg){
+	static tf::TransformBroadcaster br;
+	tf::Transform transform;
+	transform.setOrigin(tf::Vector3(msg->x, msg->y, 0.0));
+	tf::Quaternion q;
+	q.setRPY(0, 0, msg->theta);
+	transform.setRotation(q);
+	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "my_tf_broadcaster");
+	turtle_name = argv[1];
+	
+	ros::NodeHandle nh;
+	ros::Subscriber sub = nh.subscribe(turtle_name+"/pose", 10, &poseCallback);
+	
+	ros::spin();
+	return 0;
+};
